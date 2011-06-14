91a637c600d95675e17090a792063e6a388b11b0
Eric Rochester <err8n@virginia.edu>
2011-06-14 09:32:27 -0400

    Added MySQL setup to Omeka recipe.

---
+++ omeka/metadata.rb
@@ -0,0 +1,10 @@
+
+maintainer "Eric Rochester"
+maintainer_email "err8n@virginia.edu"
+license "Apache 2.0"
+description "Installs and configures Omeka."
+version "0.0.1"
+depends "mysql"
+
+recipe "omeka", "Installs and configures Omeka."
+
---
+++ omeka/recipes/default.rb
@@ -0,0 +1,25 @@
+
+include_recipe "mysql::server"
+
+node.set_unless['omeka']['mysql_user']     = 'omeka'
+node.set_unless['omeka']['mysql_password'] = 'omeka'
+node.set_unless['omeka']['mysql_db']       = 'omeka'
+
+mysql_database "create-omeka-user" do
+  host      "localhost"
+  username  "root"
+  password  node['mysql']['server_root_password']
+  database  "mysql"
+  sql       "GRANT ALL ON #{node['omeka']['mysql_db']}.* TO '#{node['omeka']['mysql_user']}'@'%' IDENTIFIED BY '#{node['omeka']['mysql_password']}';"
+  action    :query
+end
+
+mysql_database "create-omeka-db" do
+  host      "localhost"
+  username  "root"
+  password  node['mysql']['server_root_password']
+  database  "mysql"
+  sql       "CREATE DATABASE #{node['omeka']['mysql_db']} CHARACTER SET = 'utf8' COLLATE = 'utf8_unicode_ci';"
+  action    :query
+end
+
