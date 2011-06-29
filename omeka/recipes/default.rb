d2c0a6b74201e332fe78706a72c834342f3ebe5f
Eric Rochester <err8n@virginia.edu>
2011-06-29 16:52:18 -0400

    Added testing options and actions to the Omeka recipe.

--- omeka/recipes/default.rb
+++ omeka/recipes/default.rb
@@ -12,6 +12,10 @@ node.set_unless[:omeka][:mysql_db]       = 'omeka'
 node.set_unless[:omeka][:mysql_prefix]   = 'omeka_'
 node.set_unless[:omeka][:omeka_dir]      = '/vagrant/omeka'
 
+node.set_unless[:omeka][:test_user]      = node[:omeka][:mysql_user]
+node.set_unless[:omeka][:test_password]  = node[:omeka][:mysql_password]
+node.set_unless[:omeka][:test_db]        = node[:omeka][:mysql_db]
+
 # Set up the PHP MySQL package.
 mysql_pkg = value_for_platform(
     [ "centos", "redhat", "fedora" ] => {"default" => "php53-mysql"}, 
@@ -37,6 +41,17 @@ mysql_database "create-omeka-db" do
   database  "mysql"
   sql       "CREATE DATABASE #{node[:omeka]['mysql_db']} CHARACTER SET = 'utf8' COLLATE = 'utf8_unicode_ci';"
   action    :query
+end
+
+if node[:omeka][:test_db] != node[:omeka][:mysql_db]
+  mysql_database "create-omeka-test-db" do
+    host      "localhost"
+    username  "root"
+    password  node['mysql']['server_root_password']
+    database  "mysql"
+    sql       "CREATE DATABASE #{node[:omeka][:test_db]} CHARACTER SET = 'utf8' COLLATE = 'utf8_unicode_ci';"
+    action    :query
+  end
 end
 
 mysql_database "create-omeka-user-local" do
@@ -75,6 +90,50 @@ mysql_database "grant-omeka-user-remote" do
   action    :query
 end
 
+if node[:omeka][:test_user] != node[:omeka][:mysql_user]
+
+  mysql_database "create-omeka-user-local" do
+    host      "localhost"
+    username  "root"
+    password  node['mysql']['server_root_password']
+    database  "mysql"
+    sql       "CREATE USER '#{node[:omeka][:test_user]}'@'localhost' IDENTIFIED BY '#{node[:omeka][:test_password]}';"
+    action    :query
+  end
+
+  mysql_database "create-omeka-user-remote" do
+    host      "localhost"
+    username  "root"
+    password  node['mysql']['server_root_password']
+    database  "mysql"
+    sql       "CREATE USER '#{node[:omeka][:test_user]}'@'%' IDENTIFIED BY '#{node[:omeka][:test_password]}';"
+    action    :query
+  end
+
+end
+
+if node[:omeka][:test_user] != node[:omeka][:mysql_user] or node[:omeka][:test_db] != node[:omeka][:mysql_db]
+
+  mysql_database "grant-omeka-user-local" do
+    host      "localhost"
+    username  "root"
+    password  node['mysql']['server_root_password']
+    database  "mysql"
+    sql       "GRANT ALL PRIVILEGES ON #{node[:omeka][:test_db]}.* TO '#{node[:omeka][:test_user]}'@'localhost';"
+    action    :query
+  end
+
+  mysql_database "grant-omeka-user-remote" do
+    host      "localhost"
+    username  "root"
+    password  node['mysql']['server_root_password']
+    database  "mysql"
+    sql       "GRANT ALL PRIVILEGES ON #{node[:omeka][:test_db]}.* TO '#{node[:omeka][:test_user]}'@'%';"
+    action    :query
+  end
+
+end
+
 # Set up the site in Apache.
 template "#{node[:apache][:dir]}/sites-available/default" do
   source "default-site.erb"
@@ -87,6 +146,14 @@ end
 # Create the Omeka DB settings file.
 template "#{node[:omeka][:omeka_dir]}/db.ini" do
   source "db.ini.erb"
+  owner "root"
+  group "root"
+  mode 0644
+end
+
+# Create the Omeka test settings file.
+template "#{node[:omeka][:omeka_dir]}/application/tests/config.ini" do
+  source "config.ini.erb"
   owner "root"
   group "root"
   mode 0644
