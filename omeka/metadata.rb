092405ca9d918196fb1b7a133d2f9a58ccc8f86b
Eric Rochester <err8n@virginia.edu>
2011-06-30 16:13:09 -0400

    Added README files and metadata for the Omeka recipe.

--- omeka/metadata.rb
+++ omeka/metadata.rb
@@ -4,7 +4,15 @@ maintainer_email "err8n@virginia.edu"
 license "Apache 2.0"
 description "Installs and configures Omeka."
 version "0.0.1"
+
+depends "apache2"
+depends "php"
 depends "mysql"
+depends "imagemagick"
+
+%w{ centos }.each do |os|
+  supports os
+end
 
 recipe "omeka", "Installs and configures Omeka."
 
