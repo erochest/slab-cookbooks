c4d672ec923ef8601674e73dbd7335ab5dcc33f7
Eric Rochester <err8n@virginia.edu>
2011-07-11 12:02:15 -0400

    Added documentation and default attribute values for downloading Omeka themes and plugins from Git.

--- omeka/attributes/default.rb
+++ omeka/attributes/default.rb
@@ -11,13 +11,23 @@
 # Copyright   2010 The Board and Visitors of the University of Virginia
 # License     http://www.apache.org/licenses/LICENSE-2.0.html Apache 2 License
 
+default[:omeka][:omeka_dir]      = '/vagrant/omeka'
 default[:omeka][:version]        = nil
+default[:omeka][:themes]         = [
+  {:name => 'minimalist',     :url => 'git://github.com/omeka/theme-minimalist.git'},
+  {:name => 'rhythm',         :url => 'git://github.com/omeka/theme-rhythm.git'},
+  {:name => 'seasons',        :url => 'git://github.com/omeka/theme-seasons.git'}
+]
+default[:omeka][:plugins]        = [
+  {:name => 'Coins',          :url => 'git://github.com/omeka/plugin-Coins.git'},
+  {:name => 'ExhibitBuilder', :url => 'git://github.com/omeka/plugin-ExhibitBuilder.git'},
+  {:name => 'SimplePages',    :url => 'git://github.com/omeka/plugin-SimplePages.git'}
+]
 
 default[:omeka][:mysql_user]     = 'omeka'
 default[:omeka][:mysql_password] = 'omeka'
 default[:omeka][:mysql_db]       = 'omeka'
 default[:omeka][:mysql_prefix]   = 'omeka_'
-default[:omeka][:omeka_dir]      = '/vagrant/omeka'
 
 default[:omeka][:test_user]      = 'omeka'
 default[:omeka][:test_password]  = 'omeka'
