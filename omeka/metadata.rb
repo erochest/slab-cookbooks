53700e1f197f40bf918b575c6d960d52c1569dee
Eric Rochester <err8n@virginia.edu>
2011-07-21 15:45:45 -0400

    Bumped the version number of the Omeka recipe to 1.0.0.

--- omeka/metadata.rb
+++ omeka/metadata.rb
@@ -16,7 +16,7 @@ maintainer_email "err8n@virginia.edu"
 license          "Apache 2.0"
 description      "Installs and configures Omeka."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.mkd'))
-version          "0.1.0"
+version          "1.0.0"
 
 depends "apache2"
 depends "php"
