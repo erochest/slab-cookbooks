2da37b306a2ae03b3e70744358e5b86819a04ff5
Eric Rochester <err8n@virginia.edu>
2011-09-22 16:57:51 -0400

    Initial version of geonetwork recipe.

---
+++ geonetwork/README.md
@@ -0,0 +1,6 @@
+
+# Overview
+
+This installs [GeoServer Open Source][1].
+
+[1]: http://geonetwork-opensource.org/ 'GeoNetwork Open Source'
---
+++ geonetwork/recipes/default.rb
@@ -0,0 +1,33 @@
+
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at http://www.apache.org/licenses/LICENSE-2.0 Unless required by
+# applicable law or agreed to in writing, software distributed under the
+# License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+# OF ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+#
+# Author      Eric Rochester <err8n@virginia.edu>
+# Copyright   2011 The Board and Visitors of the University of Virginia
+# License     http://www.apache.org/licenses/LICENSE-2.0.html Apache 2 License
+
+require_recipe 'java'
+
+remote_file '/tmp/geonetwork-installer.jar' do
+  source node.geonetwork.installer_url
+  action :create
+end
+
+cookbook_file '/tmp/geonetwork-installer.xml' do
+  source 'geonetwork-installer.xml'
+  action :create
+end
+
+execute 'install-geonetwork' do
+  command 'java -jar /tmp/geonetwork-installer.jar /tmp/geonetwork-installer.xml'
+  user 'root'
+  action :run
+end
+
+# TODO: put behind tomcat 6
+
